c1cb250c70fe0ee4533bcf89fcac6a6c9042d8c5
Ahmed Fouad <dev@depkey.com>
2024-07-25 18:45:29 +0300

    Add project files.

---
+++ src/AbpSolution1.HttpApi.Host/AbpSolution1BrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace AbpSolution1;
+
+[Dependency(ReplaceServices = true)]
+public class AbpSolution1BrandingProvider : DefaultBrandingProvider
+{
+    public override string AppName => "AbpSolution1";
+}
